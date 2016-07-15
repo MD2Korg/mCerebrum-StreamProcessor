f6043b4160fc1cd296efe19336e91b3ad35a9f49
joy1130 <nsleheen@memphis.edu>
2016-07-15 13:50:36 -0500

    Add feature description for puffMarker

--- streamprocessor/src/main/java/org/md2k/streamprocessor/output/PuffLabel.java
+++ streamprocessor/src/main/java/org/md2k/streamprocessor/output/PuffLabel.java
@@ -13,6 +13,7 @@ import java.util.HashMap;
 /*
  * Copyright (c) 2015, The University of Memphis, MD2K Center
  * - Syed Monowar Hossain <monowar.hossain@gmail.com>
+ * - Nazir Saleheen <nazir.saleheen@gmail.com>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
